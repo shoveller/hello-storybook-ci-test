d5a5c5677b248c41e896e3857fea838292620eb5
cinos81 <cinos81@gmail.com>
2025-01-05 06:24:31 +0900

    feat: update deploy workflow

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,11 @@
 import type { Preview } from "@storybook/react";
 
+declare global {
+  interface Window {
+    __test: boolean;
+  }
+}
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -10,5 +16,8 @@ const preview: Preview = {
     },
   },
 };
+
+// 테스트 환경을 위한 전역 변수 설정
+window.__test = true;
 
 export default preview;
