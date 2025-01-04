7feaf5cf867443ea3d070f7c8d4f22a148a5e1d2
cinos81 <cinos81@gmail.com>
2025-01-05 05:08:10 +0900

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<App />} />
+),{ basename: '/hello-storybook-ci-test' })
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
